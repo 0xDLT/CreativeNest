5dcbed93e861b4bb61662a50022b76b6be9a527b
root <30773517+0xDLT@users.noreply.github.com>
2024-09-14 20:03:21 +0300

    Modified

--- edit_profile.php
+++ edit_profile.php
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
-    <link rel="stylesheet" href="profile.css">
+    <link rel="stylesheet" href="edit_profile.css">
     <?php include 'head.php'; ?>
 </head>
 <body>
